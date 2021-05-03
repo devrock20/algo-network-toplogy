c6ced66bbcf6e86f35ce2762cac630e8939aae89
devrock20 <satyadev9691@gmail.com>
2021-05-02 22:39:52 -0400

    inital commit

---
+++ network_topology.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import random
+
+
+class Graph():
+    def __init__():
+        # initialize nodes,edges,weights and adjancey matrix
+
+    def read_graph():
+        pass
+
+    def compute_node_deg():
+        pass
+
+    def compute_strength_distibution():
+        pass
+
+    def compute_avg_weight():
+        pass
+
+    def compute_charectristic_path():
+        pass
+
+    def compute_betweeness_centrality():
+        pass
+
+    def check_small_scale_property():
+        pass
---
+++ run_network.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np 
+import argparse
+from network_topology import Graph
+
+
+if __name__ == "__main__":
+    # take folder dir from command prompt
+    # call graph object
+    # read each grp  file and output centrality measures
+    pass
