d6c548414b7058d97a59bbc157f48b87a5e3646e
devrock20 <satyadev9691@gmail.com>
2021-05-05 21:28:07 -0400

    First 3 char

--- network_topology.py
+++ network_topology.py
@@ -3,26 +3,74 @@ import random
 
 
 class Graph():
-    def __init__():
-        # initialize nodes,edges,weights and adjancey matrix
+    def __init__(self,grp_file):
+        self.nodes = []
+        self.edges = []
+        with open(grp_file) as file:
+            for s in file.readlines():
+                if ')' in s and 'edge' not in s:
+                    self.nodes.append(int(s.split(')')[0]))
+                elif 'edge' not in s and '*' not in s:
+                    self.edges.append((s.split('\n')[0]))
+                else:
+                    pass
+        self.no_nodes = len(self.nodes)
+        self.no_edges = len(self.edges)
+        self.weight_matrix = np.zeros((self.no_nodes+1,self.no_nodes+1))
+        self.adjancey_matrix = np.zeros((self.no_nodes+1,self.no_nodes+1))
+        self.populate_matrix()
+        self.compute_node_deg()
+        self.compute_strength_distibution()
+        self.compute_avg_weight()
 
-    def read_graph():
+    def populate_matrix(self):
+        for edge in self.edges:
+            i = int(edge.split('-')[0])
+            j = int(edge.split('-')[1])
+            self.adjancey_matrix[i][j] = 1
+            self.weight_matrix[i][j] = random.randint(0,10)
+        
+    def compute_node_deg(self):
+        node_dict = {}
+        for i in range(self.no_nodes+1):
+            node_deg = 0
+            for j in range(self.no_nodes+1):
+                node_deg += self.adjancey_matrix[i][j]
+            node_dict["Node : {0}".format(i)]  = "Degree : {0}".format(node_deg)
+        print(node_dict)
+
+    def compute_strength_distibution(self):
+        strength_dict = {}
+        for i in range(self.no_nodes+1):
+            strength = 0
+            for j in range(self.no_nodes+1):
+                strength += self.adjancey_matrix[i][j] * self.weight_matrix[i][j]
+            strength_dict["Node : {0}".format(i)]  = "Strength : {0}".format(strength)
+        print(strength_dict)
+
+
+    def compute_avg_weight(self):
+        total_weight = 0
+        avg_weight =  {}
+        for i in range(self.no_nodes+1):
+            for j in range(self.no_nodes+1):
+                total_weight += self.weight_matrix[i][j]
+        for i  in range(self.no_nodes+1):
+            node_weight = 0
+            for j in range(self.no_nodes+1):
+                node_weight += self.weight_matrix[i][j]
+            avg_weight[i]=node_weight/total_weight
+        print(avg_weight)
+        
+
+    def compute_dis_nodes(self):
         pass
 
-    def compute_node_deg():
+    def compute_betweeness_centrality(self):
         pass
 
-    def compute_strength_distibution():
+    def check_small_scale_property(self):
         pass
 
-    def compute_avg_weight():
-        pass
-
-    def compute_charectristic_path():
-        pass
-
-    def compute_betweeness_centrality():
-        pass
-
-    def check_small_scale_property():
-        pass
+if __name__ == "__main__":
+    g = Graph('Data/Bacillus_subtilis_168/-beta--D-glucuronide_degradation.grp')
--- run_network.py
+++ run_network.py
@@ -4,8 +4,14 @@ import argparse
 from network_topology import Graph
 
 
-if __name__ == "__main__":
-    # take folder dir from command prompt
-    # call graph object
-    # read each grp  file and output centrality measures
-    pass
+if __name__== "__main__":
+    parser = argparse.ArgumentParser(description='Takes .grp folder as input')
+    parser.add_argument('--src',
+                        help='specify the folder directory')
+    args = parser.parse_args()
+    folder_dir = args.src
+    grp_files = os.listdir(folder_dir)
+    for file in grp_files:
+        print("Reading Graph {0}/{1}".format(folder_dir/file))
+        graph = Graph(file)
+    
